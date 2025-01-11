7c9fe06d0186ff259023c62e7bba014000442423
harsheeka <yoyoharshikaa@gmail.com>
2025-01-11 12:50:01 +0530

    added nodemon and script in package.json

--- src/app.js
+++ src/app.js
@@ -2,10 +2,18 @@ const express = require("express");
 
 const app = express();
 
-app.use((req,res)=>{
-    res.send("Helloooo");
+
+app.use("/test",(req,res)=>{
+    res.send("Routes are working");
+});
+app.use("/hello",(req,res)=>{
+    res.send("nodemon check");
 });
 
+app.use("/",(req,res)=>{
+    res.send("Helloooo");
+}); //generic routes to be added at last
+
 app.listen("7777", ()=> {
-    "Server is listening"
+   console.log("Server is listening");
 });
