7b64188cbc4da3cb0813ed78edb5e54ad7723d5b
harsheeka <yoyoharshikaa@gmail.com>
2025-01-26 21:46:29 +0530

    to write error handlers

--- src/app.js
+++ src/app.js
@@ -2,9 +2,15 @@ const express = require("express");
 const adminAuth = require("./middlewares/auth");
 const app = express();
 
-app.use("/admin", adminAuth, (req,res,)=>{
+app.use("/admin", adminAuth, (req,res)=>{
     res.send("hello");
 });
+
+app.use("/", (err,req,res,next)=>{
+    if(err){
+        res.status(500).send("error found");
+    }
+})
 
 app.listen("7777", ()=> {
    console.log("Server is listening");
