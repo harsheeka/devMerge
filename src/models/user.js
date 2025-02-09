aabef6fc16c029a589322846bd34a287e29776fd
harsheeka <yoyoharshikaa@gmail.com>
2025-02-09 15:12:01 +0530

    validation for url and strong password

--- src/models/user.js
+++ src/models/user.js
@@ -44,11 +44,21 @@ const userSchema = new mongoose.Schema({
     },
     photoUrl : {
         type: String,
-        default: "https://img.freepik.com/free-psd/contact-icon-illustration-isolated_23-2151903337.jpg?t=st=1738485039~exp=1738488639~hmac=a6d4ad150136d5c05c4ed5650a2f58e17b1c3b0fb961bc89eb9f7ecdf39afc06&w=740"
+        default: "https://img.freepik.com/free-psd/contact-icon-illustration-isolated_23-2151903337.jpg?t=st=1738485039~exp=1738488639~hmac=a6d4ad150136d5c05c4ed5650a2f58e17b1c3b0fb961bc89eb9f7ecdf39afc06&w=740",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Not a URL");
+            }
+        }
     },
     password : {
         type: String,
         required: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Weak Password");
+            }
+        }
     },
 }, {
     timestamps: true,
